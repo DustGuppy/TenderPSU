ea153a1b4fb3de3e3516c8ef368659b193107b26
Chris Thens <Chris Thens>
2017-04-08 23:56:13 -0400

    ignore

---
+++ app/src/main/java/io/tender/tenderio/CardAdapter.java
@@ -0,0 +1,11 @@
+package io.tender.tenderio;
+
+/**
+ * Created by cjthe on 4/8/2017.
+ */
+
+public class CardAdapter extends ArrayAdapter<Integer> {
+
+    public CardAdapter(Context context) {
+    }
+}
